c1a6fc19dca97d8aa5a351f806abc63a831fb45a
bek000han <bekuniversity@gmail.com>
2024-04-01 15:09:01 +0100

    basic files

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamb-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
